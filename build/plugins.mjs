aabd84d6c2bbdda7cb277f446dfa2d690f918cb5
Kuroneko <18299687659@163.com>
2022-07-05 00:53:01 +0800

    no message

---
+++ build/plugins.mjs
@@ -0,0 +1,41 @@
+import chalk from 'chalk';
+import dayjs from 'dayjs';
+import {
+	getIPV4address ,
+	getPort,
+} from './utils.mjs';
+
+
+
+const port = await getPort();
+
+/**
+ * 每次打包完成后输出日志
+ */
+export class LogAtSucceed {
+	env = null;
+	constructor (env = "production") {
+		this.env = env;
+	}
+	
+	count = 0;
+	
+	onFail = (error) => {
+		console.log(chalk.red(`compiled failed at ${ dayjs().format("HH:mm:ss") }\n\r`));
+	};
+	
+	apply (compiler) {
+		this.count ++;
+		compiler.hooks.done.tap('LogAtSucceed' , (stats) => {
+			
+			if ( stats.hasErrors() ) {
+				return this.onFail(stats);
+			}
+			const whisper = this.env === "production" ? "" : ` , host : http://${ getIPV4address() }:${port}\n\r`;
+			console.log(chalk.green(`compiled successfully at ${ dayjs().format("HH:mm:ss") }${whisper}`));
+		});
+		compiler.hooks.failed.tap('LogAtSucceed' , (error) => {
+			this.onFail(error);
+		});
+	}
+}
