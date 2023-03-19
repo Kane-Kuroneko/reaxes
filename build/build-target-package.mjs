9df5617c4a4322375459dac8a58f269429324323
Kane <18299687659@163.com>
2023-03-19 18:49:31 +0800

    no message

--- build/build-target-package.mjs
+++ build/build-target-package.mjs
@@ -2,6 +2,7 @@
  * 输入要打包的repoName,将其构建.返回promise
  */
 import { repoPackages } from './entrance.mjs';
+const repoList = argv.slice( 2);
 export const buildRepo = async () => {
 	/*检测输入的repoList是否都存在*/
 	repoList.forEach((repo) => {
@@ -10,32 +11,45 @@ export const buildRepo = async () => {
 		}
 	});
 	return repoList.map(async (repo) => {
-		const repoWebpackPartialConfig = (await import(`../packages/${repo}`))
+		console.log(`../packages/${ repo }/webpack.partial.mjs`);
+		const packagePath = path.resolve(`../packages/${ repo }`);
+		
+		const repoWebpackPartialConfig = (await import(`../packages/${ repo }/webpack.partial.mjs`)).webpackConfig;
+		console.log(11111111111);
+		
 	})
 };
 
+/**
+ * 由于
+ */
+const transformWebpackRelativePath = () => {
+	
+}
+buildRepo();
+
+if(0){
+	const {} = webpack;
+	const compilersPromiseList = baseRepos.map(async (repo) => {
+		const repoPartialWebpackConfig = async () => {
+			try {
+				return (await import(`../packages/${repo}/webpack.partial.mjs`)).webpackConfig
+			}catch ( e ) {
+				return {};
+			}
+		};
+		const webpackBuildConfigWithRepo = merge()
+		return webpack_promise()
+	})
+}
 
 
-const repoList = argv.slice( 2);
-const {} = webpack;
 
-
-
-const compilersPromiseList = baseRepos.map(async (repo) => {
-	const repoPartialWebpackConfig = async () => {
-		try {
-			return (await import(`../packages/${repo}/webpack.partial.mjs`)).webpackConfig
-		}catch ( e ) {
-			return {};
-		}
-	};
-	const webpackBuildConfigWithRepo = merge()
-	return webpack_promise()
-})
-
-import { webpack_promise } from 'build/toolkit.mjs';
+import { webpack_promise } from './toolkit.mjs';
 import { merge } from 'webpack-merge';
 import { webpackBaseConfig } from './webpack.base.config.mjs';
 import { webpackBuildConfig } from './webpack.build.config.mjs';
 import webpack from 'webpack';
 import {argv} from 'process';
+import path,{} from 'path';
+debugger;
