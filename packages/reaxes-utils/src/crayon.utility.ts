93ff08958589ef4a16786369b9a0781118ebb1ab
18299687659@163.com <18299687659@163.com>
2024-12-07 13:20:19 +0800

    no message

--- packages/reaxes-utils/src/crayon.utility.ts
+++ packages/reaxes-utils/src/crayon.utility.ts
@@ -1,34 +1,43 @@
-type css = {
-	[key in keyof CSSStyleDeclaration] : CSSStyleDeclaration[key] ;
+type CSS = {
+	[key in keyof CSSStyleDeclaration]: CSSStyleDeclaration[key];
 	
 };
-type crayon = ( ( css: Partial<CSSStyleDeclaration> ) => ( ...logs:any[] ) => void ) & {
-	[key in "warn" | "info" | "log" | "error"| "debug"| "trace"]: (( ...any ) => any) & {
-	[key in string]: (...any) => any ;
-} } & {
-	[key in string]: (...any) => any ;
+type crayon = ( ( css: Partial<CSSStyleDeclaration> ) => ( ...logs: any[] ) => void ) & {
+	[key in |
+		'trace' |
+		'warn' |
+		'log' |
+		'info' |
+		'error' |
+		'debug' |
+		'assert' |
+		'clear' |
+		'count' |
+		'countReset' |
+		'dir' |
+		'dirxml' |
+		'table']: ( ( ...any ) => any ) & {
+	[key in string]: ( ...any ) => any;
+}
+} & {
+	[key in string]: ( ...any ) => any;
 };
 
 /*把驼峰css key 改为短横连*/
-const transformCSSHumpToLowercase = (key:keyof CSSStyleDeclaration) => {
-	return (key.toString().split('').reduce((accumulator,k) => {
-		if(/[A-Z]/.test(k)){
-			return accumulator.concat( '-'.concat( k.toLowerCase() ) );
-		}
-		return accumulator.concat(k);
-	},'')) as keyof CSSStyleDeclaration;
+const transformCSSHumpToLowercase = ( key: string ) => {
+	return key.replace( /([A-Z])/g , '-$1' ).toLowerCase();
 };
 /*将驼峰css key对象改为短横连*/
-const batchTransformCSSHumpToLowercase = (object:{[key in keyof CSSStyleDeclaration] : any}) => {
-	const obj = {};
-	for(const key in object){ /*@ts-ignore*/
-		obj[transformCSSHumpToLowercase(key)] = object[key];
+const batchTransformCSSHumpToLowercase = ( object: { [key in keyof CSSStyleDeclaration]: any } ) => {
+	const obj: { [key in keyof Partial<CSSStyleDeclaration>]: any } = {};
+	for( const key in object ){
+		obj[transformCSSHumpToLowercase( key )] = object[key];
 	}
 	return obj;
 };
 /**
  * chalk的山寨版:crayon 用于打印更优美的日志
- * 
+ *
  * @example 
    crayon({
       'background-color' : "red" ,
@@ -37,115 +46,120 @@ const batchTransformCSSHumpToLowercase = (object:{[key in keyof CSSStyleDeclarat
  
    crayon.warn.red('以警告的方式打印红色文字',{a:1});
    crayon.warn('以警告的方式打印',{a:1});
-   
-    
+ 
+ 
  */
 
 /* secondary是console.log第二项的cssPropertiesString*/
-const argumentsAgent = (secondary , ...args) => args.reduce((accu, arg , index ) => (accu[0] += typeof arg === "string" ? arg : (accu.push(arg),"%o"),accu) ,['%c',secondary]);
+const argumentsAgent = ( secondary , ...args ) => args.reduce( ( accu , arg , index ) => (
+	accu[0] += typeof arg === 'string' ? arg : (
+		accu.push( arg ), '%o'
+	), accu
+) , [ '%c' , secondary ] );
 
 /**
  * todo 实现crayon({...css}).group('xxx')
  */
-export const crayon : crayon = new Proxy((cssProperties:Partial<CSSStyleDeclaration> = {}) => {
+export const crayon = new Proxy( ( cssProperties: Partial<CSSStyleDeclaration> = {} ) => {
 	/*using as crayon({CSSProperties})('wanna log msg')*/
-	return (...logs) => {
-		let cssString = Object.keys(cssProperties).reduce((accumulator,key:string) => {
-			/*@ts-ignore*/
-			const res = `${accumulator}${String(transformCSSHumpToLowercase(key))}:${cssProperties[key]};`
+	return ( ...logs ) => {
+		let cssString = Object.keys( cssProperties ).reduce( ( accumulator , key: string ) => {
+			
+			const res = `${ accumulator }${ String( transformCSSHumpToLowercase( key ) ) }:${ cssProperties[key] };`;
 			return res;
-		},'');
-		if(!cssString.includes('font-weight')){
+		} , '' );
+		if( !cssString.includes( 'font-weight' ) ) {
 			cssString += `font-weight:normal;`;
 		}
-		console.groupCollapsed(...argumentsAgent( cssString,...logs ));
-		console.trace(`%c`,'font-weight:normal;');
+		console.groupCollapsed( ...argumentsAgent( cssString , ...logs ) );
+		console.trace( `%c` , 'font-weight:normal;' );
 		console.groupEnd();
 		return;
 	};
-},{
-	get : (target, propKey:string, receiver) => {
+} , {
+	get : ( target , propKey: any , receiver ) => {
 		
-		if([
-			"prototype",
-			"hasOwnProperty",
-			"toString",
-			"prototype",
-			"length",
-			"name",
-			"arguments",
-			"constructor",
-			"isPrototypeOf",
-			"apply",
-			"call",
-			"bind",
-		].includes(propKey)){
-			return target[ propKey ];
+		if( [
+			'prototype' ,
+			'hasOwnProperty' ,
+			'toString' ,
+			'prototype' ,
+			'length' ,
+			'name' ,
+			'arguments' ,
+			'constructor' ,
+			'isPrototypeOf' ,
+			'apply' ,
+			'call' ,
+			'bind' ,
+		].includes( propKey ) ) {
+			return target[propKey];
 		}
 		
-		const createColorProxy = ( type = "trace" ) => new Proxy( (...logs) => {
-			if(type === "trace"){
-				console.groupCollapsed(...argumentsAgent( `color:${ propKey };font-weight:normal;`,...logs ));
-				console.trace(`%c`,'font-weight:normal;');
+		const createColorProxy = ( type = 'trace' ) => new Proxy( ( ...logs ) => {
+			if( type === 'trace' ) {
+				console.groupCollapsed( ...argumentsAgent( `color:${ propKey };font-weight:normal;` , ...logs ) );
+				console.trace( `%c` , 'font-weight:normal;' );
 				console.groupEnd();
 				return;
 			}
-			return console[type]( ...argumentsAgent( `color:${ propKey }`,...logs ) );
+			return console[type]( ...argumentsAgent( `color:${ propKey }` , ...logs ) );
 		} , {
 			get : ( target , _propKey , receiver ) => {
 				
 				return ( ...logs ) => {
-					if(type === "trace"){
-						console.groupCollapsed(...argumentsAgent( `color:${ _propKey as string };font-weight:normal;`,...logs ));
-						console.trace(`%c`,'font-weight:normal;');
+					if( type === 'trace' ) {
+						console.groupCollapsed( ...argumentsAgent( `color:${ _propKey as string };font-weight:normal;` , ...logs ) );
+						console.trace( `%c` , 'font-weight:normal;' );
 						console.groupEnd();
 						return;
 					}
 					/*当crayon.blue('',...args)时,babel使用了apply来隐式转换扩展运算符 , 导致了proxy拿到的是'apply'而非'blue' */
-					if ( _propKey === "apply" ) {
-						const [,_logs] = logs;
-						if ( type === "trace" ) {
-							console.groupCollapsed( ...argumentsAgent( `color:${ propKey }`,..._logs )  );
+					if( _propKey === 'apply' ) {
+						const [ , _logs ] = logs;
+						if( type === 'trace' ) {
+							console.groupCollapsed( ...argumentsAgent( `color:${ propKey }` , ..._logs ) );
 							console.trace( `%c` , 'font-weight:normal;' );
 							console.groupEnd();
 							return;
 						}
-						return console[type]( ...argumentsAgent( `color:${ propKey }`,..._logs ) );
+						return console[type]( ...argumentsAgent( `color:${ propKey }` , ..._logs ) );
 					}
-					return console[type]( ...argumentsAgent( `color:${ _propKey.toString() }`,...logs ) );
+					return console[type]( ...argumentsAgent( `color:${ _propKey.toString() }` , ...logs ) );
 				};
 			} ,
 		} );
-		switch ( propKey ){
-			case "trace" :
-			case "warn" : 
-			case "log" : 
-			case "info" : 
-			case "error" :
-			case "debug" :
-				return createColorProxy(propKey);
+		const validKeys = [
+			'trace' , 'warn' , 'log' , 'info' , 'error' , 'debug' ,
+			'assert' , 'clear' , 'count' , 'countReset' , 'dir' ,
+			'dirxml' , 'table' ,
+		] as const;
+		
+		if( validKeys.includes( propKey ) ) {
+			return createColorProxy( propKey );
 		}
-		return createColorProxy("trace");
-	},
-}) as crayon;
+		
+		return createColorProxy( 'trace' );
+	} ,
+} ) as unknown as crayon;
 
 /*示例 & 测试*/
-if(0){
-	crayon({fontSize : "33px"})('erer',2342343,{a:23},'erer');
-	crayon.warn['#eee']('dsdsdsdsds',{b:222});
-	crayon.warn('dsdsdsdsds',{b:222});
-	crayon.log('dsdsdsdsds',{b:222});
-	crayon.purple('1111111111111',{b:222});
-
+if( 0 ) {
+	crayon( { fontSize : '33px' } )( 'erer' , 2342343 , { a : 23 } , 'erer' );
+	crayon.warn['#eeeeee']( 'dsdsdsdsds' , { b : 222 } );
+	crayon.warn( 'dsdsdsdsds' , { b : 222 } );
+	crayon.log( 'dsdsdsdsds' , { b : 222 } );
+	crayon.purple( '1111111111111' , { b : 222 } );
+	
 	crayon( {
-		backgroundColor : "blue" ,
-		color : "green" ,
-		fontSize : "24px",
+		backgroundColor : 'blue' ,
+		color : 'green' ,
+		fontSize : '24px' ,
 		
 		
 	} )( 'bbbbbbbbbbbbbbb' , { a : 1 } );
-	crayon({color:"purple"})('asdasd',{a:1},'fafsdsd',{b:2},undefined);
+	crayon( { color : 'purple' } )( 'asdasd' , { a : 1 } , 'fafsdsd' , { b : 2 } , undefined );
 	
-	crayon[ "#aabbcc" ]( "asdsadsd");
+	crayon['#aabbcc']( 'asdsadsd' );
 }
 
